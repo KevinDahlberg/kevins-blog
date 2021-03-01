2b58787fb8d1a43513ee8872353d17c619ee4d18
KevinDahlberg <kevin@kevindahlberg.com>
2021-02-28 21:49:52 -0600

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import {
+  FirebaseAuthConsumer,
+  FirebaseAuthProvider,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd,
+  IfFirebaseUnAuthed,
+} from "@react-firebase/auth";
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -5,22 +12,37 @@ import {
   Route,
   Link,
   useParams,
+  useHistory,
 } from "react-router-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
 import Admin from "./admin";
 import Home from "./home";
-
+import { firebaseConfig } from "./env/firebase-config";
 function App() {
+  let history = useHistory();
   return (
-    <Router>
-      <Switch>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
+      <Router>
+        <Switch>
+          <Route path="/admin">
+            <IfFirebaseAuthed>
+              {({ isSignedIn, user, providerId }) => {
+                return <Admin />;
+              }}
+            </IfFirebaseAuthed>
+            <IfFirebaseUnAuthed>
+              {() => {
+                return <div>Login</div>;
+              }}
+            </IfFirebaseUnAuthed>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </FirebaseAuthProvider>
   );
 }
 
